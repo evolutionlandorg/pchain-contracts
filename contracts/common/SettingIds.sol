271eebbecb1a8c107fd910c61a7b09f5cb8b0844
leo-xinwang <tobilon@outlook.com>
2019-06-24 00:13:13 +0800

    baseline from evolution land

---
+++ contracts/common/SettingIds.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+/**
+    Id definitions for SettingsRegistry.sol
+    Can be used in conjunction with the settings registry to get properties
+*/
+contract SettingIds {
+    bytes32 public constant CONTRACT_OBJECT_OWNERSHIP = "CONTRACT_OBJECT_OWNERSHIP";
+
+    bytes32 public constant CONTRACT_TOKEN_USE = "CONTRACT_TOKEN_USE";
+
+    bytes32 public constant CONTRACT_INTERSTELLAR_ENCODER = "CONTRACT_INTERSTELLAR_ENCODER";
+}
